734bd00c9e474d5fbe2f339f11e1001a079b65c1
NECVN-tanpn <tanpn@nec.vn>
2025-08-08 12:07:12 +0700

    feat: install react-h5-audio-player, create footer

---
+++ src/components/footer/app.footer.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useHasMounted } from "@/utils/customHooks";
+import { AppBar, Container } from "@mui/material";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+
+export default function AppFooter() {
+  const hasMounted = useHasMounted();
+
+  if (!hasMounted) return <></>;
+
+  return (
+    <AppBar
+      position="fixed"
+      color="primary"
+      sx={{
+        top: "auto",
+        bottom: 0,
+        backgroundColor: "#f2f2f2",
+      }}
+    >
+      <Container sx={{ display: "flex", gap: 8 }}>
+        <AudioPlayer
+          src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+          volume={0.5}
+          style={{
+            boxShadow: "unset",
+            backgroundColor: "#f2f2f2",
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            justifyContent: "center",
+            minWidth: 100,
+          }}
+        >
+          <div style={{ color: "#ccc" }}>Tanpn</div>
+          <div style={{ color: "#000" }}>Who am I?</div>
+        </div>
+      </Container>
+    </AppBar>
+  );
+}
