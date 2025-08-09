96f8add1145b604ea92b369dd1ffead0fcbaee9b
NECVN-tanpn <tanpn@nec.vn>
2025-08-09 12:16:21 +0700

    feat: create useWavesurfer hooks=

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,23 +1,23 @@
 "use client";
+import { useWavesurfer } from "@/utils/customHooks";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
-import WaveSurfer from "wavesurfer.js";
+import { useEffect, useRef, useMemo } from "react";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 export default function WaveTrack() {
   const audioParams = useSearchParams();
   const filename = audioParams.get("audio");
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (containerRef?.current) {
-      WaveSurfer.create({
-        container: containerRef.current,
-        waveColor: "rgb(200, 0, 200)",
-        progressColor: "rgb(100, 0, 100)",
-        url: `/api?audio=${filename}`,
-      });
-    }
-  }, []);
+  const optionMemo = useMemo(() => {
+    return {
+      waveColor: "rgb(200, 0, 200)",
+      progressColor: "rgb(100, 0, 100)",
+      url: `/api?audio=${filename}`,
+    };
+  }, [filename]);
+
+  const wavesurfer = useWavesurfer(containerRef, optionMemo);
 
   return (
     <div ref={containerRef} style={{ width: "100%", height: "128px" }}>
