08c959ecbef7a499fbe669daeec035f03a3aa30d
NECVN-tanpn <tanpn@nec.vn>
2025-08-18 08:21:33 +0700

    feat: create static and dynamic metadata for SEO

--- src/app/(user)/like/page.tsx
+++ src/app/(user)/like/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sound Cloud - Like Page",
+  description: "like page of sound cloud",
+};
+
 export default function LikePage() {
   return <div>Like Page</div>;
 }
--- src/app/(user)/track/[slug]/page.tsx
+++ src/app/(user)/track/[slug]/page.tsx
@@ -2,6 +2,29 @@ import WaveTrack from "@/components/track/wave.track";
 import { sendRequest } from "@/utils/api";
 import Container from "@mui/material/Container";
 import { useSearchParams } from "next/navigation";
+import type { Metadata, ResolvingMetadata } from "next";
+
+type IGenerateMetadataProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: IGenerateMetadataProps): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+
+  // fetch data
+  const res = await sendRequest<IBackendRes<ITrackTop>>({
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/tracks/${slug}`,
+    method: "GET",
+  });
+
+  return {
+    title: res.data?.title,
+    description: res.data?.description,
+  };
+}
 
 const DetailTrackPage = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params;
